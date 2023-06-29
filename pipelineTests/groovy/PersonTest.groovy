3b2c4fad4bb5d315752d0feb45ebc47213f43863
eukazakov <evgenii.kazakov@dataiku.com>
2023-06-30 00:29:50 +0200

    add vars

--- pipelineTests/groovy/PersonTest.groovy
+++ pipelineTests/groovy/PersonTest.groovy
@@ -6,7 +6,6 @@ class PersonTest {
 
     @Test
     void testHello() {
-        def pers = [name: "John", surname: "Doe", age: 35]
         def persObj = new PersonID("John", "Doe", 35)
         assert new Person().getPerson(persObj) == "John"
     }
---
+++ vars/sayHello.groovy
@@ -0,0 +1,3 @@
+def call(String name = 'human') {
+    println "Hello, $name."
+}
