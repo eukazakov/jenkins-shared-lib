b39efcc11e17f5c1fb49e33c979d19a05b01c6f0
eukazakov <evgenii.kazakov@dataiku.com>
2023-07-04 01:08:40 +0200

    add json function

---
+++ src/foo/bar/PersonID.groovy
@@ -0,0 +1,11 @@
+package foo.bar
+
+class PersonID {
+    String name, surname
+    int age
+    PersonID(String name, String surname, int age) {
+        this.name = name
+        this.surname = surname
+        this.age = age
+    }
+}
